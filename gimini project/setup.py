7cb2e5075a063cb9c273a1b04fc8d93097715e6c
AFZjjoo <khale7843@gmail.com>
2026-07-08 16:13:05 +0400

    Update setup.py

--- gimini project/setup.py
+++ gimini project/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="GeminiProject",
     version="1.0",
-    author=" aboldazl moheb ellahi ",
+    author="Aboldazl moheb elahi",
     py_modules=["main"]
 )
